6f71381015d467c0477b9ebee179d35884a97967
Millena Bomtempo <millena.bomtempo@gmail.com>
2023-01-09 08:19:27 -0300

    challenge done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,21 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const users = this.usersRepository.list();
+    const currentUser = users.find((user) => user.id === user_id);
+
+    if (currentUser.admin === false) {
+      throw new Error("Current user is not admin.");
+    }
+
+    return users;
   }
 }
 
